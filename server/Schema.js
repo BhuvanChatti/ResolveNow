81bbed45e08046be255629f12c06dc6714be821f
BhuvanChatti <bhuvanchatti579@gmail.com>
2025-06-27 01:09:38 +0530

    Update Schema.js

--- server/Schema.js
+++ server/Schema.js
@@ -1,69 +1,41 @@
-const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
+import mongoose from "mongoose";
 
-/////////////////user///////////////////////////////
-const userSchema = mongoose.Schema({
-  name: { type: String, required: 'Name is require' },
-  email: { type: String, required: 'Email is require' },
-  password: { type: String, required: 'Password is require' },
-  phone: { type: Number, required: 'Phone is require' }, 
-  userType: { type: String, required: 'UserType is require' },
-},
-{
-  timestamps: true,
-});
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: 'Name is required' },
+  email: { type: String, required: 'Email is required' },
+  password: { type: String, required: 'Password is required' },
+  phone: { type: Number, required: 'Phone is required' },
+  userType: { type: String, required: 'UserType is required' },
+}, { timestamps: true });
 
-// userSchema.pre("save", async function (next) {
-//   try {
-//     if (!this.isModified("password")) {
-//       return next();
-//     }
-//     const hashedPassword = bcrypt.hash(this.password, 10);
-//     this.password = hashedPassword;
-//     next();
-//   } catch (error) {
-//     return next(error);
-//   }
-// });
+export const UserSchema = mongoose.model("user_Schema", userSchema);
 
-const UserSchema = mongoose.model("user_Schema", userSchema);
-
-///////////////complaint///////////////////
-const complaintSchema = mongoose.Schema({
-  userId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "user_Schema" },
-  name: {type: String, required: true },
+const complaintSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "user_Schema" },
+  name: { type: String, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
   state: { type: String, required: true },
-  pincode: { type: Number, required: true }, 
+  pincode: { type: Number, required: true },
   comment: { type: String, required: true },
   status: { type: String, required: true },
 });
 
-const ComplaintSchema = mongoose.model("complaint_schema", complaintSchema)
+export const ComplaintSchema = mongoose.model("complaint_schema", complaintSchema);
 
-///////////assigned complaint schema////////////////////////
-const assignedComplaint = mongoose.Schema({
-  agentId : {type: mongoose.Schema.Types.ObjectId, required: true, ref: "user_Schema" },
-  complaintId : {type: mongoose.Schema.Types.ObjectId, required: true, ref: "complaint_schema" },
-  status: {type: String, required: true },
-  agentName: {type: String, required: true },
-})
+const assignedComplaintSchema = new mongoose.Schema({
+  agentId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "user_Schema" },
+  complaintId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "complaint_schema" },
+  status: { type: String, required: true },
+  agentName: { type: String, required: true },
+});
 
-const AssignedComplaint = mongoose.model("assigned_complaint",assignedComplaint)
+export const AssignedComplaint = mongoose.model("assigned_complaint", assignedComplaintSchema);
 
-////////////////////chatWindow schema/////////////////////////
 const messageSchema = new mongoose.Schema({
-  name: {type: String, required: 'name is required'},
-  message: {type: String, required: 'message is required'},
-  complaintId: {type: mongoose.Schema.Types.ObjectId, ref: "assigned_complaint"}
+  name: { type: String, required: 'name is required' },
+  message: { type: String, required: 'message is required' },
+  complaintId: { type: mongoose.Schema.Types.ObjectId, ref: "assigned_complaint" }
 }, { timestamps: true });
 
-const MessageSchema = mongoose.model('message', messageSchema);
-
-module.exports = {
-  UserSchema,
-  ComplaintSchema,
-  AssignedComplaint,
-  MessageSchema,
-};
+export const MessageSchema = mongoose.model('message', messageSchema);
