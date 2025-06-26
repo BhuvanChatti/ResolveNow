7629450efb5e72c315fad26c9d4fe12bec240f05
BhuvanChatti <bhuvanchatti579@gmail.com>
2025-06-27 00:09:26 +0530

    Almost done

---
+++ server/config.js
@@ -0,0 +1,6 @@
+import mongoose from ("mongoose");
+
+mongoose.connect("mongodb+srv://bhuvanchatti579:999999999@resolvenow.cp5zsxv.mongodb.net/")
+.then(()=>{
+   console.log("connected to mongodb")
+})
